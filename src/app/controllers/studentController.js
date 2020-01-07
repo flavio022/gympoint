d07d7408718f473113350c959b5e9ec0bab8c3a2
Flavio <flavio.danilo022@gmail.com>
2020-01-07 07:20:58 -0300

    inserting userController and user routes

--- src/app/controllers/studentController.js
+++ src/app/controllers/studentController.js
@@ -4,11 +4,21 @@ import * as Yup from "yup";
 import Student from "../models/Student";
 
 class StudentController {
+  async index(req, res) {
+    const { page = 1, quantity = 20 } = req.params;
+
+    const students = await Student.findAll({
+      limit: quantity,
+      offset: (page - 1) * quantity
+    });
+    return res.json(students);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       email: Yup.string().required(),
-      idade: Yup.date().required(),
+      idade: Yup.number().required(),
       peso: Yup.number().required(),
       altura: Yup.number().required()
     });
@@ -26,6 +36,30 @@ class StudentController {
       req.body
     );
     return res.json({ id, name, email, idade, peso, altura });
+  }
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string(),
+      idade: Yup.number(),
+      peso: Yup.number(),
+      altura: Yup.number()
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation fails" });
+    }
+    const { id } = req.params;
+    const { email, idade, peso, altura } = req.body;
+
+    const student = await Student.findByPk(id);
+    if (student.email !== email) {
+      const studentExists = await Student.findOne({ where: { email } });
+      if (studentExists) {
+        return res.status(401).json({ error: "Email already in use" });
+      }
+    }
+    await student.update({ email, idade, peso, altura });
+    await student.save();
+    return res.json(student);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
+import authHeader from "./app/middlewares/auth";
 import SessionController from "./app/controllers/sessionController";
 import StudentController from "./app/controllers/studentController";
+import UserController from "./app/controllers/userController";
 const routes = new Router();
 
 routes.post("/sessions", SessionController.store);
+routes.use(authHeader);
 routes.post("/student", StudentController.store);
+routes.get("/student", StudentController.index);
+routes.put("/student/:id", StudentController.update);
+routes.get("/user", UserController.index);
+routes.post("/user", UserController.store);
+routes.put("/user/:id", UserController.update);
+
 export default routes;
